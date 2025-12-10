abcc27911a4129e7edabf778f64613cbca547cc2
bothaina <bm2400764@tkh.edu.eg>
2025-12-10 02:24:36 +0200

    round keys

--- keys.py
+++ keys.py
@@ -1,2 +1,15 @@
 from tables import pc1,pc2,shift_sched
 from functions import left_shift,permute
+
+def round_keys(main_key):
+    sub_key = permute(main_key,pc1)
+    left = sub_key[:28]
+    right = sub_key[28:]
+    final_key = []
+
+    for s in shift_sched:
+        left = left_shift(left, s)
+        right = left_shift (right , s)
+        full = left + right 
+        final_key.append(permute(full,pc2))
+    return final_key 
