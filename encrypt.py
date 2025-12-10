e87cb7eaa81f9f7af81f239df6302341a5d9a0e3
bothaina <bm2400764@tkh.edu.eg>
2025-12-10 04:48:19 +0200

    encrypting

--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,14 @@
 from tables import initial_permutation,final_permutation
 from functions import permute,xor
 from f_function import fiestel
+
+def encrypt(text,key):
+    block = permute(text,initial_permutation)
+    l = block[:32]
+    r = block[32:]
+    for i in range (16):
+        fiestel_result = fiestel(r,key[i])
+        l,r=r,xor(l,fiestel_result)
+    
+        final = r + l
+        return permute(final, final_permutation)
