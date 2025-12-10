70709a6685e0ee3c4ad86abe9602af73dc7dca1b
bothaina <bm2400764@tkh.edu.eg>
2025-12-10 22:33:16 +0200

    comments

--- encrypt.py
+++ encrypt.py
@@ -3,12 +3,12 @@ from functions import permute,xor
 from f_function import feistel
 
 def encrypt(text,key):
-    block = permute(text,initial_permutation)
-    l = block[:32]
-    r = block[32:]
-    for i in range (16):
-        feistel_result = feistel(r,key[i])
-        l,r=r,xor(l,feistel_result)
+    block = permute(text,initial_permutation) #performs initial permutation
+    l = block[:32] #left side
+    r = block[32:] #right side
+    for i in range (16): #loop for 16 rounds
+        feistel_result = feistel(r,key[i]) 
+        l,r=r,xor(l,feistel_result) #the new l is old r and the new r is the old l XORed with feistel result
     
-    final = r + l
-    return permute(final, final_permutation)
+    final = r + l #swapping left and right
+    return permute(final, final_permutation) #final permutation
--- f_function.py
+++ f_function.py
@@ -4,15 +4,15 @@ from tables import expand,s_boxes,p_box
 def s_box_substitutions(input_bits):
     result = ""
     for i in range(8): 
-        blocks = input_bits[i*6:(i+1)*6]
+        blocks = input_bits[i*6:(i+1)*6] #divides bits into groups of 6
         column = int(blocks[1:5],2) # the middle four blocks identifies which column
         row = int (blocks[0]+blocks[5],2) #first and last bit identifies which row 
-        value = s_boxes[i][row][column]
-        result +=f"{value:04b}"
+        value = s_boxes[i][row][column] #gets the value from the s boxes
+        result +=f"{value:04b}" #converts the value to 4 bits and padded with zeros
     return result
 
 def feistel(right_half, key):
-    expanded = permute(right_half,expand)
-    x = xor(key,expanded)
+    expanded = permute(right_half,expand) #performs expansion permutation
+    x = xor(key,expanded) #XORs the 
     subs = s_box_substitutions(x)
     return permute(subs,p_box)
