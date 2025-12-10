933c8371b377deeb49eee1be7145fcdd6972dc9c
bothaina <bm2400764@tkh.edu.eg>
2025-12-10 04:26:23 +0200

    s box subs

--- f_function.py
+++ f_function.py
@@ -1,3 +1,16 @@
 from functions import permute,xor
 from tables import expand,s_boxes,p_box
 
+def s_box_substitutions(input_bits):
+    result = ""
+    for i in range(8):
+        blocks = input_bits[i*6:(i+1)*6]
+        column = int(blocks[1:5],2)
+        row = int (blocks[0]+blocks[5],2)
+        value = s_boxes[i][row][column]
+        result +=f"{value:04b}"
+    return result
+    
+def fiestel(right_half, key):
+    expanded = permute(right_half,expand)
+    x = xor(key,expanded)
