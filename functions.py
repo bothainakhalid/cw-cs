cd4a5eab0ed6a277ccee43fc231881b50f8d006e
bothaina <bm2400764@tkh.edu.eg>
2025-12-10 21:14:45 +0200

    comments

--- functions.py
+++ functions.py
@@ -1,14 +1,19 @@
 def permute(block,table):
     return "".join(block[i-1] for i in table)
+#permutation function
 
 def xor(a,b):
     return "".join("1" if a[i]!=b[i] else "0" for i in range (len(a)))
+#xor function
 
 def left_shift(bits, num):
     return bits[num:]+bits[:num]
+#left shift function for key generation 
 
 def bin_to_hexa(b):
     return f"{int(b,2):016X}"
+#converting binary to hexadecimal
 
 def hexa_to_bin(h):
     return f"{int(h,16):064b}"
+#converting hexadecimal to binary
--- keys.py
+++ keys.py
@@ -6,6 +6,8 @@ def round_keys(main_key):
     left = sub_key[:28]
     right = sub_key[28:]
     final_key = []
+#performs pc1 on the key which drops 8 parity keys making it 56 bits,
+# then splits bits into left and right
 
     for s in shift_sched:
         left = left_shift(left, s)
@@ -13,3 +15,4 @@ def round_keys(main_key):
         full = left + right 
         final_key.append(permute(full,pc2))
     return final_key 
+#performs shifting then performs pc2 to generate the final round key
