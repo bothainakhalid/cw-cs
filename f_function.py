eaf5819ba23cdf4c5bb10370d29b1077267b91c0
bothaina <bm2400764@tkh.edu.eg>
2025-12-10 21:12:55 +0200

    some comments

--- f_function.py
+++ f_function.py
@@ -3,10 +3,10 @@ from tables import expand,s_boxes,p_box
 
 def s_box_substitutions(input_bits):
     result = ""
-    for i in range(8):
+    for i in range(8): 
         blocks = input_bits[i*6:(i+1)*6]
-        column = int(blocks[1:5],2)
-        row = int (blocks[0]+blocks[5],2)
+        column = int(blocks[1:5],2) # the middle four blocks identifies which column
+        row = int (blocks[0]+blocks[5],2) #first and last bit identifies which row 
         value = s_boxes[i][row][column]
         result +=f"{value:04b}"
     return result
